0ae3aec114d8c5379f4c0e8eb283e72c644ae7d3
Pham Quoc, Vinh <vinh.phamquoc@lazada.com>
2020-01-14 10:10:37 +0700

    [master]:MahavArray Check - Reformat code

--- src/main/java/org/vinh/mahav/MahavArrayCheck.java
+++ src/main/java/org/vinh/mahav/MahavArrayCheck.java
@@ -4,32 +4,33 @@ package org.vinh.mahav;
  * Created by vinh.phamquoc on 1/8/20
  */
 public class MahavArrayCheck {
-	public int isMahavArray(int[] array){
+	public int isMahavArray(int[] array) {
 		if (array.length < 2) {
 			return 0;
 		}
-		int sum = 0;
-		int end = 2;
-		boolean isNotMahavLength = false;
-		boolean isMaha = false;
-		int count = 0;
-		for(int k = 1; k <= array.length*2; k++) {
-			if (array.length == k*(k+1)/2){
+		int sum = 0,
+				count = 0,
+				end = 2;
+		boolean isNotMahavLength = false,
+				isMaha = false;
+
+		for (int k = 1; k <= array.length * 2; k++) {
+			if (array.length == k * (k + 1) / 2) {
 				isNotMahavLength = true;
 			}
 		}
-		 if (!isNotMahavLength) {
-		 	return 0;
-		 }
+		if (!isNotMahavLength) {
+			return 0;
+		}
 
 		for (int i = 0; i < array.length; i++) {
 
-			if (i > 0){
+			if (i > 0) {
 				sum += array[i];
 				count++;
 			}
 
-			if(end == count && sum == array[0]) {
+			if (end == count && sum == array[0]) {
 				sum = 0;
 				count = 0;
 				end += 1;
