873d831c49f862f31ec5e6a3919a6e7e98860e49
Pham Quoc, Vinh <vinh.phamquoc@lazada.com>
2020-01-14 10:06:48 +0700

    [master]:MahavArray Check

---
+++ src/test/java/org/vinh/AbstractTest.java
@@ -0,0 +1,11 @@
+package org.vinh;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * Created by vinh.phamquoc on 1/8/20
+ */
+@ExtendWith(MockitoExtension.class)
+public class AbstractTest {
+}
