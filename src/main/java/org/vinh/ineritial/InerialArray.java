f432200f5bc67d5ee89687c02084247d15e3b52a
Pham Quoc, Vinh <vinh.phamquoc@lazada.com>
2020-02-04 00:44:56 +0700

    [master]:Inerial Array

---
+++ src/main/java/org/vinh/ineritial/InerialArray.java
@@ -0,0 +1,41 @@
+package org.vinh.ineritial;
+
+/**
+ * Created by vinh.phamquoc on 1/14/20
+ */
+public class InerialArray {
+	public boolean isInerialArray(int[] array) {
+
+		if (array.length <= 1) {
+			return false;
+		}
+
+		boolean atLeastOneOdd = false;
+		Integer minOdd = null,
+				maxValue = array[0];
+
+		for (int value : array) {
+			if (value > maxValue) {
+				maxValue = value;
+			}
+			if (value % 2 != 0) {
+				atLeastOneOdd = true;
+				if (minOdd == null || value < minOdd) {
+					minOdd = value;
+				}
+			}
+		}
+
+		return atLeastOneOdd && maxValue % 2 == 0 && checkAnyOddLargerThanEvent(minOdd, maxValue, array) ;
+	}
+
+	private boolean checkAnyOddLargerThanEvent(Integer minOdd, int maxValue, int[] array){
+		for (int k : array) {
+			if (k % 2 == 0 && minOdd != null && minOdd < k && k != maxValue) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
