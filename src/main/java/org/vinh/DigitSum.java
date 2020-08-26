f9bfb55d8dd5a4c16b61bef14a01be8c4aeac355
Pham Quoc, Vinh <vinh.phamquoc@lazada.com>
2020-08-27 00:18:50 +0700

    ArrayFilter and DigitSum

---
+++ src/main/java/org/vinh/DigitSum.java
@@ -0,0 +1,25 @@
+package org.vinh;
+
+/**
+ * Created by vinh.phamquoc on 8/26/20
+ */
+//Write a function named isDigitSum that returns 1 if sum of all digits of the first argument is less than the second argument and 0 otherwise.
+//For example isDigitSum(32121,10 ) would return 1 because 3+2+1+2+1 = 9 < 10.
+
+//More examples:
+//isDigitSum(32121,9) returns 0, isDigitSum(13, 6) returns 1, isDigitSum(3, 3) returns 0
+
+//The function should return -1 if either argument is negative, so isDigitSum(-543, 3) returns -1.
+public class DigitSum {
+	public int isDigitSum(int i, int j){
+		if (i < 0 || j < 0) {
+			return 1;
+		}
+		int total = 0;
+		while (i > 0) {
+			total += i % 10;
+			i/=10;
+		}
+		return total < j ? 1: 0;
+	}
+}
---
+++ src/test/java/org/vinh/array/ArrayFilterTest.java
@@ -0,0 +1,45 @@
+package org.vinh.array;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.vinh.AbstractTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by vinh.phamquoc on 8/25/20
+ */
+class ArrayFilterTest extends AbstractTest {
+	@InjectMocks
+	ArrayFilter arrayFilter;
+	@Test
+	public  void testCase1(){
+		assertEquals(1, arrayFilter.isFilter(new int[]{1, 2, 3, 9, 6, 11}));
+	}
+	@Test
+	public  void testCase2(){
+		assertEquals(1, arrayFilter.isFilter(new int[]{3, 4, 6, 7, 14, 16}));
+	}
+	@Test
+	public  void testCase3(){
+		assertEquals(1, arrayFilter.isFilter(new int[]{3, 4, 6, 7, 14, 16}));
+	}
+	@Test
+	public  void testCase4(){
+		assertEquals(0, arrayFilter.isFilter(new int[]{9, 6, 18}));
+	}
+	@Test
+	public  void testCase5(){
+		assertEquals(0, arrayFilter.isFilter(new int[]{4, 7, 13}));
+	}
+	@Test
+	public  void testCase6(){
+		assertEquals(1, arrayFilter.isFilter(new int[]{4, 9, 11, 7}));
+	}
+	@Test
+	public  void testCase7(){
+		assertEquals(1, arrayFilter.isFilter(new int[]{4, 9, 11, 13}));
+	}
+
+
+}
