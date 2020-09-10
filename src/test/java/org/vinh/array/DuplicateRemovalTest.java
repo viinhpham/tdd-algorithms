140ce9e900cb5de0b44d84328de6b90cbdea33a9
Pham Quoc, Vinh <vinh.phamquoc@lazada.com>
2020-09-10 23:09:33 +0700

    done for MIU part

---
+++ src/test/java/org/vinh/array/DuplicateRemovalTest.java
@@ -0,0 +1,26 @@
+package org.vinh.array;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.vinh.AbstractTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author vinh.phamquoc on 9/10/20
+ */
+class DuplicateRemovalTest extends AbstractTest {
+	@InjectMocks
+	DuplicateRemoval duplicateRemoval;
+
+	@Test
+	public void testCase1(){
+		assertEquals(2, duplicateRemoval.removeDuplicate(new int[]{1,1,2}));
+	}
+
+	@Test
+	public void testCase2(){
+		assertEquals(5, duplicateRemoval.removeDuplicate(new int[]{0,0,0,1,1,2,2,3,3,4}));
+	}
+
+}
